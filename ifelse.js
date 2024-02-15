96f82a710d4f54d83f1cfe399399b8f8e9b3b212
l0rrainy <142435583+l0rrainy@users.noreply.github.com>
2024-02-15 12:24:38 -0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,13 @@
+function VerificarLogin () {
+    let txtLogin = document.getElementById("txtLogin")
+    let txtSenha = document.getElementById("txtSenha")
+
+    let login = "teste";
+    let senha = "teste";
+
+    if (txtLogin.value == login && txtSenha.value == senha) {
+        console.log("logado com sucesso!");
+    } else {
+        console.log("login ou senha incorretos! Digite e tente novamente.");
+    }
+}
